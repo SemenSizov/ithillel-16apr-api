fc86a756d4b165dcd4af4a16edeb6d59b4e6a752
Semen Syzov <22974895+SemenSizov@users.noreply.github.com>
2024-06-07 21:51:32 +0300

    add api advanced lectrue

--- app.mjs
+++
@@ -1,4 +0,0 @@
-import axios from 'axios';
-
-const res = await axios.get('https://demoqa.com/BookStore/v1/Books')
-console.log(res.data)
--- each.test.js
+++
@@ -1,7 +0,0 @@
-test.each([
-    [1, 2, 3],
-    [4, 5, 9],
-    [11, 15, 26],
-])('Sum %d + %d = %d', (a, b, expected) => {
-    expect(a + b).toBe(expected)
-})
